054f1ed0ca11691012763129799b3ca490a74627
Aminelt <laatfaamine@gmail.com>
2022-11-07 17:07:46 +0100

    Login/Registration

---
+++ src/main/java/com/example/authentificationsystem/AuthentificationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.authentificationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthentificationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthentificationSystemApplication.class, args);
+    }
+
+}
